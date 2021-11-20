ad2397f752cd84edc7e264e74f53dd069e279d37
miguel-rosa <miguelgoncvs@gmail.com>
2021-11-20 16:52:24 -0300

    fix: Remove crossOrigin message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <meta name="description" content="Bora mudar junto o futuro dos processos seletivos" />
     <link rel="icon" href="/favicon-avalie.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
   </Head>
   <Component {...pageProps} />
